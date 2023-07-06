8d3b91ff3c72ac8df9c4e70a18cb53c2aaefc62b
jeanfcg <104164533+jeanfcg@users.noreply.github.com>
2023-07-06 00:00:03 -0500

    Add files via upload

---
+++ LAB1/calculadora.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+typedef double (*Operacion)(double, double);
+
+double sumar(double a, double b) {
+	return a + b;
+}
+
+double restar(double a, double b) {
+	return a - b;
+}
+
+double multiplicar(double a, double b) {
+	return a * b;
+}
+
+double dividir(double a, double b) {
+	if (b != 0) {
+		return a / b;
+	} else {
+		std::cout << "¡Error! No se puede dividir por cero." << std::endl;
+		return 0.0;
+	}
+}
+double potenciar(double a, double b) {
+	return pow(a, b);
+}
+double radicar(double a, double b) {
+	return pow(a, 1 / b);
+}
+int main()
+{
+	typedef double (*Operacion)(double, double);
+	Operacion operadores[6] = {&::sumar, &::restar, &::multiplicar, &::dividir, &::potenciar, &::radicar};
+	float a, b, c; int opc;
+	cout << "ingrese el primer numero: ", cin >> a;
+	cout << "ingrese el segundo numero: ", cin >> b;
+	cout << "\n0.suma \n1.resta \n2.multiplicacion \n3.division \n4.potencia \n5.raiz \n\ningrese la opcion: ", cin >> opc;
+	c = operadores[opc](a,b);
+	cout<<"la respuesta es: "<<c;
+}
---
+++ LAB1/calculadora_clases.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+class Calculadora {
+public:
+	static double sumar(double a, double b) {
+		return a + b;
+	}
+	
+	static double restar(double a, double b) {
+		return a - b;
+	}	
+	
+	static double multiplicar(double a, double b) {
+		return a * b;
+	}
+	
+	static double dividir(double a, double b) {
+		if (b != 0) {
+			return a / b;
+		} else {
+			cout << "¡Error! No se puede dividir por cero."<<endl;
+			return 0.0;
+		}
+	}
+	static double potenciar(double a, double b) {
+		return pow(a, b);
+	}
+	
+	static double radicar(double a, double b) {
+		return pow(a, 1 / b);
+	}
+};
+int main() {
+	Calculadora O;
+	float a, b, c;
+	cout << "ingrese el primer numero: ", cin >> a;
+	cout << "ingrese el segundo numero: ", cin >> b;
+	double (*fOperaciones[6])(double a, double b) = {&O.sumar, &O.restar, &O.multiplicar, &O.dividir, &O.potenciar, &O.radicar};
+	char operadores[] = "+-*/^V";
+	for (size_t i = 0; i < 6; i++)
+	{
+		c = fOperaciones[i](a, b);
+		cout << a << " " << operadores[i] << " " << b << " = " << c << endl;
+	}
+}
