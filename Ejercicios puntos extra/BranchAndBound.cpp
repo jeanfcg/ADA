b948cf889e2346aa56e169772771256c86c3b571
jeanfcg <104164533+jeanfcg@users.noreply.github.com>
2023-08-11 20:08:21 -0500

    Add files via upload

---
+++ Ejercicios puntos extra/BranchAndBound.cpp
@@ -0,0 +1,66 @@
+/*
+Problema: Algoritmo B&B para resolver el problema de la asignación con la siguiente matriz de costes:
+x  f1 f2 f3 f4
+d1 94 1 54 68
+d2 74 10 88 82
+d3 62 88 8 76
+d4 11 74 81 21
+*/
+
+
+#include <iostream>
+#include <algorithm>
+#include <climits>
+#include <vector>
+
+
+using namespace std;
+
+const int INF = INT_MAX;
+
+int costMin = INF;
+vector<int> mejorAssignment;
+
+int a = 4; // num tareas y trabajadores
+vector<vector<int>> costs = {
+	{94, 1, 54, 68},
+{74, 10, 88, 82},
+	{62, 88, 8, 76},
+{11, 74, 81, 21}
+};
+
+
+
+void branchAndBound(int trabajador, vector<bool>& assigned, vector<int>& assignment, int cost) {
+	if (trabajador == a) {
+		if (cost < costMin) {
+			costMin = cost;
+			mejorAssignment = assignment;
+		}
+		return;
+	}
+	
+	for (int tarea = 0; tarea < a; ++tarea) {
+		if (!assigned[tarea]) {
+			assigned[tarea] = true;
+			assignment[trabajador] = tarea;
+			branchAndBound(trabajador + 1, assigned, assignment, cost + costs[trabajador][tarea]);
+			assigned[tarea] = false;
+		}
+	}
+}
+
+int main() {
+	vector<bool> assigned(a, false);
+	vector<int> assignment(a);
+	
+	branchAndBound(0, assigned, assignment, 0);
+	
+	cout << "la asignación óptima es: ";
+	for (int trabajador = 0; trabajador < a; ++trabajador) {
+		cout << "f" << (trabajador + 1) << "->d" << (mejorAssignment[trabajador] + 1) << " ";
+	} cout << endl;
+	cout << "el costo mínimo es: " << costMin << endl;
+	
+	return 0;
+}
---
+++ Ejercicios puntos extra/poda.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> longestIncreasingSubsequence(const vector<int>& numeros) {
+	int n = numeros.size();
+	vector<int> longitudes(n, 1); // Inicialmente, cada posición es una secuencia de longitud 1
+	
+	for (int i = 1; i < n; ++i) {
+		for (int j = 0; j < i; ++j) {
+			if (numeros[i] > numeros[j] && longitudes[i] < longitudes[j] + 1) {
+				longitudes[i] = longitudes[j] + 1;
+			}
+		}
+	}
+	
+	int maxLongitud = 0, endIndex = 0;
+	for (int i = 0; i < n; ++i) {
+		if (longitudes[i] > maxLongitud) {
+			maxLongitud = longitudes[i];
+			endIndex = i;
+		}
+	}
+	
+	vector<int> lis;
+	while (maxLongitud > 0) {
+		lis.insert(lis.begin(), numeros[endIndex]);
+		--maxLongitud;
+		--endIndex;
+		while (endIndex >= 0 && longitudes[endIndex] != maxLongitud) {
+			--endIndex;
+		}
+	}
+	
+	return lis;
+}
+
+int main() {
+	vector<int> numeros = {11, 17, 5, 8, 6, 4, 7, 12, 3};
+	
+	vector<int> lis = longestIncreasingSubsequence(numeros);
+	
+	cout << "la secuencia creciente de máxima longitud es: ";
+	for (int num : lis) {
+		cout << num << " ";
+	}
+	cout << endl;
+	
+	return 0;
+}
